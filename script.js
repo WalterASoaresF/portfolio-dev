d712c3c22c4a256a8dafb7a1fa06f9a56302ecbf
Walter Soares <waltersoaresbes@gmail.com>
2026-03-19 18:57:31 -0300

    key interests update

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const menu = document.getElementById("menu");
 toggle.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+const card = document.getElementById("flipcard");
+
+card.addEventListener("click", () => {
+  card.classList.toggle("flip");
+});
