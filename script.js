c2d25d23e3f3a1ab7a832d4aceb4e971d09c69c7
Walter Soares <waltersoaresbes@gmail.com>
2026-03-23 11:40:12 -0300

    v1 portfolio and update in design
    add some colors, root and Desktop model

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const menu = document.getElementById("menu");
 toggle.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+const interests = document.querySelectorAll(".interest");
+const textBox = document.querySelector("#text .title p");
+
+const defaultText = textBox.textContent;
+
+interests.forEach(item => {
+  item.addEventListener("mouseenter", () => {
+    const text = item.querySelector("p").textContent;
+    textBox.textContent = text;
+  });
+
+  item.addEventListener("mouseleave", () => {
+    textBox.textContent = defaultText;
+  });
+});
